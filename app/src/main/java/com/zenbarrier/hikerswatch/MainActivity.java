3af2fca514066ff5028c60020b5b7dd5bbc628bb
ZenBarrier <anthony.j.barrera@gmail.com>
2016-10-03 15:15:27 -0400

    Get Location information

--- app/src/main/java/com/zenbarrier/hikerswatch/MainActivity.java
+++ app/src/main/java/com/zenbarrier/hikerswatch/MainActivity.java
@@ -3,7 +3,9 @@ package com.zenbarrier.hikerswatch;
 import android.Manifest;
 import android.app.Service;
 import android.content.pm.PackageManager;
+import android.location.Address;
 import android.location.Criteria;
+import android.location.Geocoder;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
@@ -11,10 +13,15 @@ import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
+
 public class MainActivity extends AppCompatActivity implements LocationListener {
 
     LocationManager locationManager;
     String provider;
+    Geocoder geocoder;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +30,18 @@ public class MainActivity extends AppCompatActivity implements LocationListener 
 
         locationManager = (LocationManager) getSystemService(Service.LOCATION_SERVICE);
         provider = locationManager.getBestProvider(new Criteria(), false);
+        geocoder = new Geocoder(getApplicationContext(), Locale.getDefault());
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        onLocationChanged(locationManager.getLastKnownLocation(provider));
     }
 
     @Override
@@ -59,7 +78,21 @@ public class MainActivity extends AppCompatActivity implements LocationListener 
 
     @Override
     public void onLocationChanged(Location location) {
-
+        double lat = location.getLatitude();
+        double lng = location.getLongitude();
+        double accuracy = location.getAccuracy();
+        double speed = location.getSpeed();
+        double bearing = location.getBearing();
+        double altitude = location.getAltitude();
+        List<Address> addressList;
+        try {
+            addressList = geocoder.getFromLocation(lat, lng, 1);
+            if(addressList.size() > 0){
+                Address address = addressList.get(0);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
